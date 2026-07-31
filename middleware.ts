b7c5cfb1e083523d74c782b80c211262cb59b03c
alecxander567 <alecxanderespaldon21@gmail.com>
2026-07-31 10:45:26 +0800

    Fix error builds

--- middleware.ts
+++
@@ -1,4 +0,0 @@
-// This file has been migrated to proxy.ts following the Next.js v16
-// deprecation of the `middleware` convention. See proxy.ts for the
-// active route protection logic.
-export {};
