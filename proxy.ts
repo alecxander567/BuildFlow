0bc3b4691d354a05c5eb3b25c56605be376396f7
alecxander567 <alecxanderespaldon21@gmail.com>
2026-07-31 22:49:18 +0800

    Commit

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ function isPublicRoute(pathname: string): boolean {
   );
 }
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const session = request.cookies.get("session");
 
