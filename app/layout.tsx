bb9e3f6d5caca5e89487f118c394b1dec6a095b6
alecxander567 <alecxanderespaldon21@gmail.com>
2026-08-01 09:58:30 +0800

    Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 5,
+  userScalable: true,
+};
+
 import ThemeInitializer from "./components/ThemeInitializer";
 
 export default function RootLayout({
